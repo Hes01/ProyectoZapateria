dacff762e740269ef1509aad1aae9bd8f6e4688e
elvis <elvishuancaflores@gmail.com>
2024-08-19 22:38:09 -0500

    form proveedor y clientes

--- WinFormsApp1/AddCliente.vb
+++ WinFormsApp1/AddCliente.vb
@@ -48,53 +48,104 @@ Public Class AddCliente
         btnRegistrar.Visible = False
     End Sub
 
+
     Private Sub RegistrarCliente()
         Dim tipoPersona As String = cmbTipo.SelectedItem.ToString()
-        Dim idPersona As Integer = GetNextIdPersona()
-        Dim personaData As String
+        Dim idPersona As Integer = ObtenerUltimoIdPersona() + 1
+        Dim idPersonaNatural As Integer = ObtenerUltimoIdPersonaNatural() + 1
+        Dim idPersonaJuridica As Integer = ObtenerUltimoIdPersonaJuridica() + 1
         Dim detalleData As String = ""
-
-        'IdPersona|Telefono|Correo|Direccion|Ruc|TipoPersona|Estado
-        ' Registrar la información común en el archivo persona.txt
-        personaData = $"{idPersona}|{txtTelefono.Text}|{txtEmail.Text}|{txtDirección.Text}|{txtRuc.Text}|{"Cliente"}|1"
-        Using sw As StreamWriter = New StreamWriter("persona.txt", True)
-            sw.WriteLine(personaData)
-        End Using
-
 
         Select Case tipoPersona
             Case "Juridica"
                 'IdPersonaJuridica|RazonSocial|IdPersona|Estado
-                detalleData = $"{idPersonaJuridica}|{txtRazonSocial.Text}|{idPersona}"
-                Using sw As StreamWriter = New StreamWriter("personaJuridica.txt", True)
+                detalleData = $"{idPersonaJuridica}|{txtRazonSocial.Text}|{idPersona}|True"
+                Using sw As StreamWriter = New StreamWriter(PersonaJuridica.RutaArchivo, True)
                     sw.WriteLine(detalleData)
                 End Using
 
             Case "Natural"
                 'IdPersonaNatural|Nombre|Apellido|Dni|IdPersona|Estado
-                detalleData = $"{idPersonaNatural}|{txtNombres.Text}|{txtApellidos.Text}|{txtDni.Text}|{idPersona}"
-                Using sw As StreamWriter = New StreamWriter("personaNatural.txt", True)
+                detalleData = $"{idPersonaNatural}|{txtNombres.Text}|{txtApellidos.Text}|{txtDni.Text}|{idPersona}|True"
+                Using sw As StreamWriter = New StreamWriter(PersonaNatural.RutaArchivo, True)
                     sw.WriteLine(detalleData)
                 End Using
         End Select
 
+        ' Registrar los datos comunes en el archivo Persona
+        'IdPersona|Telefono|Correo|Direccion|Ruc|TipoPersona|Estado
+        Dim personaData As String = $"{idPersona}|{txtTelefono.Text}|{txtEmail.Text}|{txtDirección.Text}|{txtRuc.Text}|{"Cliente"}|True"
+        Using sw As StreamWriter = New StreamWriter(Persona.RutaArchivo, True)
+            sw.WriteLine(personaData)
+        End Using
+
         MessageBox.Show("Cliente registrado correctamente.", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Information)
     End Sub
 
-    Private Function GetNextIdPersona() As Integer
-        ' Obtener el siguiente ID disponible para la nueva persona
-        Dim maxId As Integer = 0
-        If File.Exists("persona.txt") Then
-            Dim lines() As String = File.ReadAllLines("persona.txt")
-            If lines.Length > 0 Then
-                maxId = lines.Select(Function(line) Integer.Parse(line.Split("|"c)(0))).Max()
-            End If
+    Private Function ObtenerUltimoIdPersona() As Integer
+        Dim ultimoId As Integer = 0
+
+        If File.Exists(Persona.RutaArchivo) Then
+            Using sr As New StreamReader(Persona.RutaArchivo)
+                Dim linea As String
+                While Not sr.EndOfStream
+                    linea = sr.ReadLine()
+                    Dim datos() As String = linea.Split("|"c)
+                    Dim id As Integer
+                    If Integer.TryParse(datos(0), id) Then
+                        ultimoId = Math.Max(ultimoId, id)
+                    End If
+                End While
+            End Using
         End If
-        Return maxId + 1
+
+        Return ultimoId
+    End Function
+
+    Private Function ObtenerUltimoIdPersonaNatural() As Integer
+        Dim ultimoId As Integer = 0
+
+        If File.Exists(PersonaNatural.RutaArchivo) Then
+            Using sr As New StreamReader(PersonaNatural.RutaArchivo)
+                Dim linea As String
+                While Not sr.EndOfStream
+                    linea = sr.ReadLine()
+                    Dim datos() As String = linea.Split("|"c)
+                    Dim id As Integer
+                    If Integer.TryParse(datos(0), id) Then
+                        ultimoId = Math.Max(ultimoId, id)
+                    End If
+                End While
+            End Using
+        End If
+
+        Return ultimoId
+    End Function
+
+    Private Function ObtenerUltimoIdPersonaJuridica() As Integer
+        Dim ultimoId As Integer = 0
+
+        If File.Exists(PersonaJuridica.RutaArchivo) Then
+            Using sr As New StreamReader(PersonaJuridica.RutaArchivo)
+                Dim linea As String
+                While Not sr.EndOfStream
+                    linea = sr.ReadLine()
+                    Dim datos() As String = linea.Split("|"c)
+                    Dim id As Integer
+                    If Integer.TryParse(datos(0), id) Then
+                        ultimoId = Math.Max(ultimoId, id)
+                    End If
+                End While
+            End Using
+        End If
+
+        Return ultimoId
     End Function
 
     Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
         Me.Close()
+        Cliente.Show()
+
     End Sub
 End Class
 
--- WinFormsApp1/My Project/Application.Designer.vb
+++ WinFormsApp1/My Project/Application.Designer.vb
@@ -33,7 +33,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.WinFormsApp1.AddCliente
+            Me.MainForm = Global.WinFormsApp1.Proveedor
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
